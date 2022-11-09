a160f56c89eb8044de258afd958afabd51e6ffdd
Jarred Allen <jarred@moveparallel.com>
2022-11-08 16:19:25 -0800

    Add extension trait for easier construction

--- README.md
+++ README.md
@@ -2,16 +2,23 @@ hex-display
 ----
 [![crates.io](https://img.shields.io/crates/v/hex-display.svg)](https://crates.io/crates/hex-display)
 
-An implementation of Display on a wrapper for `&[u8]` which provides a hexdump (see [`Hex`]).
-This crate also works in `no_std` environments.
+An implementation of Display on a wrapper for `&[u8]` which provides a hexdump (see [`Hex`] and
+[`HexDisplayExt`]). This crate also works in `no_std` environments.
+
+If `std` is present, it can also convert to a hexdump as a string.
 
 ## Example usage
 
 ```rust
-use hex_display::Hex;
+use hex_display::HexDisplayExt;
 
 assert_eq!(
-    format!("{}", Hex(&[0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef])),
+    format!("{}", [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef].hex()),
+    "0123456789abcdef"
+);
+#[cfg(feature = "std")]
+assert_eq!(
+    [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef].hex_string(),
     "0123456789abcdef"
 );
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,58 @@
 #![no_std]
-
 #![doc = include_str!("../README.md")]
 
 use core::fmt::Display;
 
+#[cfg(feature = "std")]
+extern crate std;
+
+/// An extension trait that allows for more easily constructing [`Hex`] values
+///
+/// ```
+/// use hex_display::HexDisplayExt;
+/// assert_eq!(
+///     format!("{}", [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef].hex()),
+///     "0123456789abcdef"
+/// );
+/// assert_eq!(
+///     format!("{}", [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef].upper_hex()),
+///     "0123456789ABCDEF"
+/// );
+/// ```
+pub trait HexDisplayExt {
+    /// Display as a hexdump
+    fn hex(&self) -> Hex<'_>;
+
+    /// Display as a capitalized hexdump
+    fn upper_hex(&self) -> UpperHex<'_> {
+        self.hex().upper()
+    }
+
+    /// Convert to a lower-case hex string
+    ///
+    /// Only present when built with `std` support.
+    #[cfg(feature = "std")]
+    fn hex_string(&self) -> std::string::String {
+        use std::string::ToString;
+        self.hex().to_string()
+    }
+}
+
+impl HexDisplayExt for [u8] {
+    fn hex(&self) -> Hex<'_> {
+        Hex(self)
+    }
+}
+
+impl<const N: usize> HexDisplayExt for [u8; N] {
+    fn hex(&self) -> Hex<'_> {
+        Hex(self)
+    }
+}
+
 /// A wrapper type for `&[u8]` which implements Display by providing a hexdump
+///
+/// See [`HexDisplayExt`] for an easier method of constructing this type.
 ///
 /// ```
 /// use hex_display::Hex;
@@ -17,8 +65,22 @@ use core::fmt::Display;
 #[derive(Debug)]
 pub struct Hex<'a>(
     /// The bytes to be converted into a hexdump
-    pub &'a [u8]
+    pub &'a [u8],
 );
+
+impl<'a> Hex<'a> {
+    /// Convert to upper-case hexdump
+    pub fn upper(self) -> UpperHex<'a> {
+        UpperHex(self.0)
+    }
+
+    /// Convert to lower-case hexdump
+    ///
+    /// This is a no-op.
+    pub fn lower(self) -> Self {
+        self
+    }
+}
 
 impl<'a> Display for Hex<'a> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
@@ -42,8 +104,22 @@ impl<'a> Display for Hex<'a> {
 #[derive(Debug)]
 pub struct UpperHex<'a>(
     /// The bytes to be converted into a hexdump
-    pub &'a [u8]
+    pub &'a [u8],
 );
+
+impl<'a> UpperHex<'a> {
+    /// Convert to upper-case hexdump
+    ///
+    /// This is a no-op.
+    pub fn upper(self) -> Self {
+        self
+    }
+
+    /// Convert to lower-case hexdump
+    pub fn lower(self) -> Hex<'a> {
+        Hex(self.0)
+    }
+}
 
 impl<'a> Display for UpperHex<'a> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
@@ -74,7 +150,10 @@ mod tests {
     fn test_all_byte_pairs() {
         for (a, b) in (0..=0xff).zip(0..=0xff) {
             assert_eq!(format!("{:02x}{:02x}", a, b), format!("{}", Hex(&[a, b])));
-            assert_eq!(format!("{:02X}{:02X}", a, b), format!("{}", UpperHex(&[a, b])));
+            assert_eq!(
+                format!("{:02X}{:02X}", a, b),
+                format!("{}", UpperHex(&[a, b]))
+            );
         }
     }
 }
